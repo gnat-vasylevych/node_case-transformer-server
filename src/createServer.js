794fa3f0b5f0e8ba2923e0cae1aff689ffdfc748
Vlad <vladyslav.vasylevych@gmail.com>
2025-02-24 21:55:55 +0200

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,47 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+
+const http = require('http');
+const { validateUrl } = require('./validateUrl');
+const { createSucessPayload } = require('./createSucessPayload');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const urlValidation = validateUrl(req.url);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (urlValidation.correct) {
+      respondWithSuccess(res, req.url);
+    } else {
+      responWithError(res, urlValidation.messages);
+    }
+  });
+
+  return server;
+}
+
+function respondWithSuccess(res, url) {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+
+  const payload = createSucessPayload(url);
+
+  res.end(JSON.stringify(payload));
+}
+
+function responWithError(res, messages) {
+  res.statusCode = 400;
+  res.statusMessage = 'Bad request';
+
+  const payload = {
+    errors: [...messages].map((el) => {
+      return { message: el };
+    }),
+  };
+
+  res.end(JSON.stringify(payload));
+}
+
+module.exports = { createServer };
